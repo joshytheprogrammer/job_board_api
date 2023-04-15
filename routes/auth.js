2b5700f827038570ae2d1c45a697b3ed78989d2c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-16 00:13:45 +0100

    made changes

--- routes/auth.js
+++ routes/auth.js
@@ -55,6 +55,12 @@ router.post('/signup', async (req, res) => {
   if (!username || !email || !password) {
     return res.status(400).json({ message: 'All fields are required!' });
   }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(409).json({ message: 'Email already exists' });
+  }
+
  
   // Hash the password and add the new user to the array
   const hashedPassword = bcrypt.hashSync(password, 10);

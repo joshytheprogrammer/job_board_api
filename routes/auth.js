4ae3b0eb865f0a293ac9d4ec98521685e179fe28
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-04-18 08:02:15 +0100

    made changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const validateToken = require('../middleware/validateToken')
-const User = require('../models/User')
+const validateToken = require('../middleware/validateToken');
+const User = require('../models/User');
 
 router.post('/login', async (req, res) => {
   // Check if username and password are provided
--- routes/job.js
+++ routes/job.js
@@ -1,3 +1,33 @@
 const router = require('express').Router();
+const validateToken = require('../middleware/validateToken');
+const Job = require('../models/Job');
+
+router.post('/create', validateToken, async (req, res) => {
+  const { job } = req.body;
+  const user = req.user;
+
+  if(!job) {
+    return res.status(400).json({message: 'The JOB array is required!'});
+  }
+
+  const newJob = new Job({
+    creator_id: user.id,
+    title: job.title,
+    desc: job.desc,
+    keywords: job.keywords,
+    status: "ongoing"
+  });
+
+  try {
+    await newJob.save()
+    res.status(201).json({ message: 'Job created successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/recent', async (req, res) => {
+  
+});
 
 module.exports = router

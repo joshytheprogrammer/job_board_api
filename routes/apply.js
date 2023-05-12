5cd28ce151d3ea309da3e072c178a40610d428e5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-05-12 20:52:35 +0100

    made changes

--- routes/apply.js
+++ routes/apply.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const validateToken = require('../middleware/validateToken');
 const Application = require('../models/Application');
+const Job = require('../models/Job');
+const { off } = require('../models/Tag');
 
 router.post('/create', validateToken, async (req, res) => {
   // const { application } = req.body.application;
@@ -38,18 +40,88 @@ router.get('/user/:id', validateToken, async (req, res) => {
   const user = req.user;
 
   if(userID != user.id) {
-    res.status(401).json({"message": 'Only the user that created the application can see their applications'});
-    return;
+    return res.status(401).json({"message": 'Only the user that created the application can see their applications'});
   }
 
   const applications = await Application.find({user_id: userID}, '_id job_id data status bid');
 
   if(!applications.length) {
-    res.status(200).json({"message": 'This user has not applied to any jobs'});
-    return
+    return res.status(200).json({"message": 'This user has not applied to any jobs'});
   }
 
   res.status(200).json({"applications": applications});
+});
+
+router.post('/reject/', validateToken, async (req, res) => {
+  const appID = req.body.app_id;
+  const userID = req.user.id;
+
+  // Retrieve user application details
+  const app = await Application.findById(appID, 'user_id job_id status');
+
+  // Return error if application is not found
+  if(!app) {
+    return res.status(400).json({"message": 'No application with that ID exists!'});
+  }
+
+  // Retrieve job details
+  const jobID = app.job_id;
+  const job = await Job.findById(jobID, 'creator_id');
+
+  // Check if user is the job creator
+  if(userID != job.creator_id) {
+    return res.status(400).json({"message": 'Only the user that created an application can reject it!'});
+  }
+
+  if(!await Application.findByIdAndUpdate(appID, {status: "rejected"})){
+    return res.status(400).json({"message": 'An error occurred. Contact administrator!!!'});
+  }
+
+  res.status(200).json({message: "Application rejected successfully"});
+});
+
+router.post('/accept/', validateToken, async (req, res) => {
+  const appID = req.body.app_id;
+  const userID = req.user.id;
+
+  // Retrieve user application details
+  const app = await Application.findById(appID, 'user_id job_id status');
+
+  // Return error if application is not found
+  if(!app) {
+    return res.status(400).json({"message": 'No application with that ID exists!'});
+  }
+
+  // Retrieve job details
+  const jobID = app.job_id;
+  const job = await Job.findById(jobID, 'creator_id');
+
+  // Check if user is the job creator
+  if(userID != job.creator_id) {
+    return res.status(400).json({"message": 'Only the user that created an application can reject it!'});
+  }
+
+  if(!await Application.findByIdAndUpdate(appID, {status: "accepted"})){
+    return res.status(400).json({"message": 'An error occurred. Contact administrator!!!'});
+  }
+
+  res.status(200).json({message: "Application accepted successfully"});
+
+  // Reject all Applications when one is accepted
+  try{
+    await Application.updateMany({job_id: jobID, status: "pending"}, {status: "rejected"});
+    // It is at this point you need to enter all rejections into the DB. For rejection reasons.
+  }catch(e) {
+    console.log("Attempt to reject all applications - failed. This may cause errors.");
+    console.log({error: e});
+  }
+
+  // Close Job when one application is rejected
+  try{
+    await Job.findOneAndUpdate(jobID, {status: "closed"});
+  }catch(e) {
+    console.log("Attempt to close job failed. This may cause inconsitencies!");
+  }
 });
 
 module.exports = router
--- routes/auth.js
+++ routes/auth.js
@@ -37,7 +37,7 @@ router.post('/login', async (req, res) => {
   }
 
   // Generate tokens
-  const accessToken = jwt.sign({ user: data }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' });
+  const accessToken = jwt.sign({ user: data }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10080m' });
   const refreshToken = jwt.sign({ user: data }, process.env.REFRESH_TOKEN_SECRET);
 
   return res.status(200).json({ accessToken, refreshToken });

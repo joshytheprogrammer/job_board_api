bf97d3869b57047fe6bfda270074eefc98c2766b
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-05-30 22:05:53 +0100

    fixed error in } package.json

--- ml/models/jobRecommendation.js
+++ ml/models/jobRecommendation.js
@@ -1,30 +1,30 @@
-// const brain = require('brain.js');
+const brain = require('brain.js');
 
-// const trainRecommendationModel = (numericData) => {
-//   const trainingData = numericData.map(job => {
-//     const input = job.features;
-//     const output = job.status === 'ongoing' ? 1 : 0;
-//     return { input, output };
-//   });
+const trainRecommendationModel = (numericData) => {
+  const trainingData = numericData.map(job => {
+    const input = job.features;
+    const output = job.status === 'ongoing' ? 1 : 0;
+    return { input, output };
+  });
 
-//   const net = new brain.NeuralNetwork({
-//     learningRate: 0.1, // Adjust the learning rate as needed
-//     iterations: 1000, // Increase the number of iterations if necessary
-//     errorThresh: 0.005, // Set a smaller error threshold to define when to stop training
-//   });
+  const net = new brain.NeuralNetwork({
+    learningRate: 0.1, // Adjust the learning rate as needed
+    iterations: 1000, // Increase the number of iterations if necessary
+    errorThresh: 0.005, // Set a smaller error threshold to define when to stop training
+  });
 
-//   net.train(trainingData);
+  net.train(trainingData);
 
-//   return net;
-// };
+  return net;
+};
 
-// const generateRecommendations = (userPreferences, model) => {
-//   console.log("\n Generating...")
+const generateRecommendations = (userPreferences, model) => {
+  console.log("\n Generating...")
 
-//   return userPreferences.map(preference => {
-//     const prediction = model.run(preference);
-//     return { job: jobData[prediction], score: prediction };
-//   });
-// };
+  return userPreferences.map(preference => {
+    const prediction = model.run(preference);
+    return { job: jobData[prediction], score: prediction };
+  });
+};
 
-// module.exports = { trainRecommendationModel, generateRecommendations };
+module.exports = { trainRecommendationModel, generateRecommendations };
--- ml/utils/preprocess.js
+++ ml/utils/preprocess.js
@@ -1,54 +1,54 @@
-// const natural = require('natural');
-// const { WordTokenizer, PorterStemmer, stopwords } = natural;
+const natural = require('natural');
+const { WordTokenizer, PorterStemmer, stopwords } = natural;
 
-// const tokenizer = new WordTokenizer();
-// const stemmer = PorterStemmer;
+const tokenizer = new WordTokenizer();
+const stemmer = PorterStemmer;
 
-// const preprocessData = (jobData, vocabulary) => {
-//   return jobData.map(job => {
-//     const cleanedTitle = job.title.toLowerCase().replace(/[^\w\s]/g, '');
-//     const cleanedDesc = job.desc.toLowerCase().replace(/[^\w\s]/g, '');
+const preprocessData = (jobData, vocabulary) => {
+  return jobData.map(job => {
+    const cleanedTitle = job.title.toLowerCase().replace(/[^\w\s]/g, '');
+    const cleanedDesc = job.desc.toLowerCase().replace(/[^\w\s]/g, '');
 
-//     const titleTokens = tokenizer.tokenize(cleanedTitle);
-//     const descTokens = tokenizer.tokenize(cleanedDesc);
-//     const keywordTokens = job.keywords.map(keyword => keyword.toLowerCase());
+    const titleTokens = tokenizer.tokenize(cleanedTitle);
+    const descTokens = tokenizer.tokenize(cleanedDesc);
+    const keywordTokens = job.keywords.map(keyword => keyword.toLowerCase());
 
-//     const filteredTitle = titleTokens.filter(token => !stopwords.includes(token));
-//     const filteredDesc = descTokens.filter(token => !stopwords.includes(token));
-//     const filteredKeywords = keywordTokens.filter(token => !stopwords.includes(token));
+    const filteredTitle = titleTokens.filter(token => !stopwords.includes(token));
+    const filteredDesc = descTokens.filter(token => !stopwords.includes(token));
+    const filteredKeywords = keywordTokens.filter(token => !stopwords.includes(token));
 
-//     const stemmedTitle = filteredTitle.map(token => stemmer.stem(token));
-//     const stemmedDesc = filteredDesc.map(token => stemmer.stem(token));
-//     const stemmedKeywords = filteredKeywords.map(token => stemmer.stem(token));
+    const stemmedTitle = filteredTitle.map(token => stemmer.stem(token));
+    const stemmedDesc = filteredDesc.map(token => stemmer.stem(token));
+    const stemmedKeywords = filteredKeywords.map(token => stemmer.stem(token));
 
-//     stemmedTitle.forEach(token => vocabulary.addDocument(token));
-//     stemmedDesc.forEach(token => vocabulary.addDocument(token));
-//     stemmedKeywords.forEach(token => vocabulary.addDocument(token));
+    stemmedTitle.forEach(token => vocabulary.addDocument(token));
+    stemmedDesc.forEach(token => vocabulary.addDocument(token));
+    stemmedKeywords.forEach(token => vocabulary.addDocument(token));
 
-//     return {
-//       ...job,
-//       titleTokens: stemmedTitle,
-//       descTokens: stemmedDesc,
-//       keywordTokens: stemmedKeywords,
-//     };
-//   });
-// };
+    return {
+      ...job,
+      titleTokens: stemmedTitle,
+      descTokens: stemmedDesc,
+      keywordTokens: stemmedKeywords,
+    };
+  });
+};
 
-// const convertToNumericData = (preprocessedData, vocabulary) => {
-//   return preprocessedData.map(job => {
-//     const { titleTokens, descTokens, keywordTokens } = job;
+const convertToNumericData = (preprocessedData, vocabulary) => {
+  return preprocessedData.map(job => {
+    const { titleTokens, descTokens, keywordTokens } = job;
     
-//     const titleVector = vocabulary.tfidfs(titleTokens);
-//     const descVector = vocabulary.tfidfs(descTokens);
-//     const keywordVector = vocabulary.tfidfs(keywordTokens);
-//     const numericVector = titleVector.concat(descVector).concat(keywordVector);
+    const titleVector = vocabulary.tfidfs(titleTokens);
+    const descVector = vocabulary.tfidfs(descTokens);
+    const keywordVector = vocabulary.tfidfs(keywordTokens);
+    const numericVector = titleVector.concat(descVector).concat(keywordVector);
 
-//     return {
-//       ...job,
-//       features: numericVector,
-//     };
-//   });
-// };
+    return {
+      ...job,
+      features: numericVector,
+    };
+  });
+};
 
 
-// module.exports = { preprocessData, convertToNumericData };
+module.exports = { preprocessData, convertToNumericData };
